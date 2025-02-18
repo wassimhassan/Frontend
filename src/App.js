e53d34331d0311df995710ca03ac6ff8cb25ef53
Ghinwaaar <ghinwa.abi.ramia2@gmail.com>
2025-02-18 20:16:30 +0200

    login/ reset-pass integrated

--- src/App.js
+++ src/App.js
@@ -3,6 +3,7 @@ import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 import WelcomePage from './components/WelcomePage';
 import SignUp from './components/SignUp';
 import Login from './components/Login';
+import ResetPassword from "./components/ResetPassword";
 
 function App() {
   return (
@@ -11,6 +12,7 @@ function App() {
         <Route path="/" element={<WelcomePage />} />
         <Route path="/signup" element={<  SignUp />} />
         <Route path="/login" element={<Login />} />
+        <Route path="/reset-password" element={<ResetPassword />} />
       </Routes>
     </Router>
   );
